f275d84bd27503f8d694c3b328db0820029ee7fd
Astana <oibas_kypchak@mail.ru>
2018-11-28 14:50:33 +0600

    search pet

--- src/main/java/view/PetsJTable.java
+++ src/main/java/view/PetsJTable.java
@@ -5,7 +5,11 @@ import model.PetTableModel;
 import model.PetType;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,7 +19,7 @@ public class PetsJTable extends JFrame {
 
     private PetTableModel pets;
     private JTable table;
-
+    private TableRowSorter<TableModel> rowSorter;
     public PetsJTable(){
 
         super("My pets store");
@@ -23,6 +27,48 @@ public class PetsJTable extends JFrame {
         pets = new PetTableModel();
 
         table = new JTable(pets);
+
+        rowSorter = new TableRowSorter<>(table.getModel());
+        table.setRowSorter(rowSorter);
+
+        JTextField jtfFilter = new JTextField();
+        JButton jbtFilter = new JButton("Filter");
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(new JLabel("Specify a word to match:"),
+                BorderLayout.WEST);
+        panel.add(jtfFilter, BorderLayout.CENTER);
+
+        jtfFilter.getDocument().addDocumentListener(new DocumentListener(){
+
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String text = jtfFilter.getText();
+
+                if (text.trim().length() == 0) {
+                    rowSorter.setRowFilter(null);
+                } else {
+                    rowSorter.setRowFilter(RowFilter.regexFilter("(?i)" + text));
+                }
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                String text = jtfFilter.getText();
+
+                if (text.trim().length() == 0) {
+                    rowSorter.setRowFilter(null);
+                } else {
+                    rowSorter.setRowFilter(RowFilter.regexFilter("(?i)" + text));
+                }
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+        });
 
         JButton addButton = new JButton("Add pet");
 
@@ -71,6 +117,7 @@ public class PetsJTable extends JFrame {
         Container container = getContentPane();
         container.add(new JScrollPane(table), BorderLayout.CENTER);
         container.add(inputPanel, BorderLayout.NORTH);
+        container.add(panel, BorderLayout.SOUTH);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(400,300);
